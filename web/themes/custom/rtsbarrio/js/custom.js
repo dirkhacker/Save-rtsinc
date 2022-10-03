544c5ae3ae309e5e69d25009807feccb132c0571
Jason Bugiada <jbugiada@MAC-011050.localdomain>
2022-10-03 13:00:53 -0400

    DM-369: Added styles and JS for form UI.

--- web/themes/custom/rtsbarrio/js/custom.js
+++ web/themes/custom/rtsbarrio/js/custom.js
@@ -10,8 +10,22 @@
   Drupal.behaviors.rtsbarrio = {
     attach: function(context, settings) {
 
-      // Custom code here
-
+      // Add the has-value class to a sibling label
+      // if the input has a value
+      let input = ".form-control";
+      $(input).on('change', function() {
+        if ($(this).val().length > 0){
+          $(this).siblings("label").addClass('has-value');
+        }
+        else {
+          $(this).siblings("label").removeClass('has-value');
+        }
+      });
+      $(input).each(function() {
+        if ($(this).val().length > 0) {
+          $(this).siblings("label").addClass('has-value');
+        }
+      });
     }
   };
 
