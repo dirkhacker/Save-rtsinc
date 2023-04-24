0137360c6641b50b64d29ee6d8a601cd93841bfc
Dirk Hacker <dhacker@ryanrts.com>
2023-04-24 16:07:22 -0500

    add modal popup JS to display forms from Contact Us button, and twig

--- web/themes/custom/rtsbarrio/js/custom.js
+++ web/themes/custom/rtsbarrio/js/custom.js
@@ -45,6 +45,39 @@
         }   
       }); 
 
+
+      //modal webform      
+      var modal = $('<div id="webform-modal" class="webform-modal"></div>');
+      var closeButton = $('<button id="webform-modal-close" class="webform-modal-close">X</button>');
+      var webformContainer = $('<div id="webform-container" class="webform-container"></div>');
+      modal.append(closeButton);
+      modal.append(webformContainer);
+      $('body').append(modal);
+
+      function closeModal() {
+        modal.hide();
+      }
+
+      closeButton.on('click', closeModal);
+
+      modal.on('click', function(event) {
+        if ($(event.target).is(modal)) {
+          closeModal();
+        }
+      });
+
+      $('#open-webform-modal').not('.webform-modal-processed').addClass('webform-modal-processed').on('click', function() {
+        // Replace 'your_webform_id' with the actual Webform ID.
+        var webformId = 'rts_multiselect';
+
+        // Load only the form element inside the webform via AJAX and display the modal.
+        webformContainer.load('/webform/' + webformId + ' form', function() {
+          modal.show();
+        });
+      });
+
+
+
     }
   };
 
